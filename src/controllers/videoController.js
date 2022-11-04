8f552c4a5aa6fbcc5cf349acd50e85a3f6c9b308
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-11-04 17:00:15 +0900

    making pages with pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,43 @@
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+const videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const edit = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing ${video.title}` });
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
