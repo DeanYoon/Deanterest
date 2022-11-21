6134e645e7cecef03322b4fef3d265d0799495b2
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-11-21 18:50:03 +0900

    ~11.1

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
@@ -13,7 +16,7 @@ module.exports = {
   watch: true,
 
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
