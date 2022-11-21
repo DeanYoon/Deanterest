c6edab61e628ccebdb33fa9e34a09af1ddcc7bb0
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-11-22 08:08:00 +0900

    11.2~

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,23 @@
-console.log("Video Player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlay = (e) => {
+  //if the video is playing, pause it
+  if (video.paused) {
+    video.play();
+    playBtn.innerText = "Pause";
+  } else {
+    video.pause();
+    playBtn.innerText = "Play";
+  }
+  // else, play it
+};
+const handleMute = (e) => {
+  // mute the volume
+};
+
+play.addEventListener("click", handlePlay);
+mute.addEventListener("click", handleMute);
