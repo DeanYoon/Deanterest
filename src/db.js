d94da7a366076acfec9e29b64858a9d55e5dd970
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-12-04 20:01:59 +0900

    mongoose connect editted

--- src/db.js
+++ src/db.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 
